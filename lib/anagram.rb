887349c46832d8ab5e084b95ba7087810d0de032
Tim Knoll <timothy.knoll@gmail.com>
2018-10-09 16:50:04 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,12 @@
 # Your code goes here!
 class Anagram
-  attr_accessor : word
+  attr_accessor :word
 
   def initialize
     @word = word
   end
 
-  def match(word_Array) 
+  def match(word_Array)
 
   end
 
